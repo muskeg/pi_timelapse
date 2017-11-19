12828dd2919ddf6cebcfdde4f8f2ec9cad715092
raph <raphaell@raph-mbpro.local>
2017-11-19 12:50:54 -0500

    added the main script file

---
+++ pi_timelapse.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+#
+# pi_timelapse: a basic timelapse script to use with
+# the Raspberry Pi's camera module built on python-picamera.
+#
+# Usage:
+#
+#
+#
+
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.parse_args()
